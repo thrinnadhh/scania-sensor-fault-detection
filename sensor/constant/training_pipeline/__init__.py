cac98aaf3f65ddb933741544d105e96c0d5da814
thrinnadhh <111868359+thrinnadhh@users.noreply.github.com>
2024-03-04 07:15:34 +0530

    Update __init__.py

--- sensor/constant/training_pipeline/__init__.py
+++ sensor/constant/training_pipeline/__init__.py
@@ -1,6 +1,7 @@
 import os
 from sensor.constant.s3_bucket import TRAINING_BUCKET_NAME
 
+
 SAVED_MODEL_DIR =os.path.join("saved_models")
 # defining common constant variable for training pipeline
 TARGET_COLUMN = "class"
