4b29a4acd4bb80f744d0b0e0ef1103912e290f7e
thrinnadhh <111868359+thrinnadhh@users.noreply.github.com>
2024-03-04 06:54:39 +0530

    Update model_pusher.py

--- sensor/components/model_pusher.py
+++ sensor/components/model_pusher.py
@@ -8,7 +8,6 @@ from sensor.ml.metric.classification_metric import get_classification_score
 from sensor.utils.main_utils import save_object,load_object,write_yaml_file
 
 import shutil
-
 class ModelPusher:
 
     def __init__(self,
